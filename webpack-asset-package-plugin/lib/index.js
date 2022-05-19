33a269b563042e13f3763f7a4da404af2853cce0
gexiaolong <1013363134@qq.com>
2022-05-19 17:21:14 +0800

    refactor: add eslint

--- webpack-asset-package-plugin/lib/index.js
+++ webpack-asset-package-plugin/lib/index.js
@@ -1,7 +1,6 @@
 const mime = require('mime-types');
 const JSZip = require('jszip');
-const RawSource = require('webpack-sources').RawSource;
-
+const { RawSource } = require('webpack-sources');
 
 class AssetPackagePlugin {
   constructor(options) {
@@ -15,17 +14,18 @@ class AssetPackagePlugin {
       fileTypes: ['html', 'js', 'css'],
       excludeFileNames: [],
       transformExtensions: /^(gz|map)$/i,
-    }
+    };
 
-    this.options = {...defaultOptions, ...options};
+    this.options = { ...defaultOptions, ...options };
   }
 
   getFileType(str) {
+    // eslint-disable-next-line no-param-reassign
     str = str.replace(/\?.*/, '');
     const split = str.split('.');
     let ext = split.pop();
     if (this.options.transformExtensions.test(ext)) {
-      ext = split.pop() + '.' + ext;
+      ext = `${split.pop()}.${ext}`;
     }
     return ext;
   }
@@ -35,11 +35,11 @@ class AssetPackagePlugin {
     const zip = new JSZip();
 
     hooks.emit.tapAsync('AssetPackagePlugin', async (compilation, callback) => {
-      const { 
-        fileTypes, 
-        packageNameKey, 
-        packageNameValue, 
-        excludeFileNames, 
+      const {
+        fileTypes,
+        packageNameKey,
+        packageNameValue,
+        excludeFileNames,
         version,
         indexFileName,
         folderName,
@@ -50,70 +50,66 @@ class AssetPackagePlugin {
       const content = {
         version,
         [packageNameKey]: packageNameValue,
-        items: []
+        items: [],
       };
 
       // add filename
-      for(const filename in compilation.assets) {
+      // eslint-disable-next-line
+      for (const filename in compilation.assets) {
         const fileType = this.getFileType(filename);
-      
-        if(excludeFileNames.includes(filename)) {
-          continue;
-        }
 
-        if(isFileTypeLimit && !fileTypes.includes(fileType)) {
-          continue;
+        if (!(
+          // 在排除文件列表内
+          excludeFileNames.includes(filename)
+          // 不再文件类型列表中
+          || (isFileTypeLimit && !fileTypes.includes(fileType))
+        )) {
+          content.items.push({
+            version,
+            [packageNameKey]: packageNameValue,
+            // TODO:
+            // remoteUrl: baseUrl + filename
+            path: filename,
+            mimeType: mime.lookup(fileType) || 'application/octet-stream',
+          });
         }
-
-        content.items.push({
-          version,
-          [packageNameKey]:
-          
-          
-           packageNameValue,
-          // TODO:
-          // remoteUrl: baseUrl + filename
-          path: filename,
-          mimeType: mime.lookup(fileType) || 'application/octet-stream'
-        })
       }
 
       const outputFile = JSON.stringify(content, null, 2);
+
       // output index.json
-      compilation.assets[indexFileName] = {
+      compilation.assets[indexFileName] = { // eslint-disable-line
         source: () => outputFile,
         size: () => outputFile.length,
-      }
+      };
 
       // create zip file
-      const folder  = zip.folder(folderName)
-
-      for(const filename in compilation.assets) {
+      const folder = zip.folder(folderName);
+      // eslint-disable-next-line
+      for (const filename in compilation.assets) {
         const fileType = this.getFileType(filename);
 
-        if(excludeFileNames.includes(filename)) {
-          continue;
+        if (!(
+          // 在排除文件列表内
+          excludeFileNames.includes(filename)
+          // 不再文件类型列表中
+          || (isFileTypeLimit && !fileTypes.includes(fileType))
+        )) {
+          const source = compilation.assets[filename].source();
+
+          folder.file(filename, source);
         }
-
-        if(isFileTypeLimit && !fileTypes.includes(fileType)) {
-          continue;
-        }
-
-        const source = compilation.assets[filename].source();
-
-        folder.file(filename, source);
       }
 
       const zipBuffer = await zip.generateAsync({
         type: 'nodebuffer',
-      })
-      
-      const outputpath = folderName + '.zip';
-      console.log(zipBuffer);
-      compilation.assets[outputpath] = new RawSource(zipBuffer)
-        
+      });
+      const outputpath = `${folderName}.zip`;
+
+      compilation.assets[outputpath] = new RawSource(zipBuffer); // eslint-disable-line
+
       callback();
-    })
+    });
   }
 }
 
